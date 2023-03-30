475cfa89ba23d2e5b73a5c1a5d661cd65ce3bca2
Lye <Lye3@mail.ru>
2023-03-30 21:42:26 +0300

    поправил резину в header

--- source/js/modules/main-banner.js
+++ source/js/modules/main-banner.js
@@ -3,6 +3,14 @@ const maxText = 'Получить бесплатную консультацию'
 const minText = 'Бесплатная консультация';
 
 const bannerTextSwap = () => {
+  window.addEventListener('load', () => {
+    if (innerWidth < 768) {
+      bannerText.textContent = minText;
+    } else {
+      bannerText.textContent = maxText;
+    }
+  });
+
   window.addEventListener('resize', () => {
     if (innerWidth < 768) {
       bannerText.textContent = minText;
