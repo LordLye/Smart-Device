deb16a904adb3fb79db040c1e0a809e6b3753501
Lye <Lye3@mail.ru>
2023-04-04 20:52:30 +0300

    добавил lazyload для фоновых изображений услуг

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,8 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+import {initAccordions} from './vendor/init-accordion';
+
+window.addEventListener('DOMContentLoaded', () => {
+  initAccordions();
+});
